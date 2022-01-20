edaeba21af6f23247123f084d68644877cc614f6
dineshRJ <dinesh@moonraft.com>
2022-01-20 23:05:10 +0530

    Server side cache added with help of memory-cache package

--- index.js
+++ index.js
@@ -2,19 +2,39 @@ const scraper = require("./scraper.js");
 var express = require("express"),
   app = express(),
   port = process.env.PORT || 3000;
+var mcache = require("memory-cache");
+
+var cache = (duration) => {
+  return (req, res, next) => {
+    let key = "__express__" + req.originalUrl || req.url;
+    let cachedBody = mcache.get(key);
+    if (cachedBody) {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(cachedBody));
+      return;
+    } else {
+      res.endResponse = res.end;
+      res.end = (body) => {
+        mcache.put(key, JSON.parse(body), duration * 1000);
+        res.endResponse(body);
+      };
+      next();
+    }
+  };
+};
 
 app.listen(port);
 
-app.get("/", async (req, res, next) => {
+app.get("/", cache(180), async (req, res, next) => {
   const result = await scraper.scrape();
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(result));
 });
 
-app.get("/:id", async (req, res, next) => {
+app.get("/:id", cache(180), async (req, res, next) => {
   const result = await scraper.scrapeByCode(req.params.id);
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(result));
 });
 
-console.log("todo list RESTful API server started on: " + port);
+console.log("Chess Ecodoe API server listening on: " + port);
